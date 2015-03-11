aa68122988293ab24b8a0cb0a7303f4810f4cb68
ivmartel-guest <ivmartel@gmail.com>
2015-03-11 23:29:45 +0100

    Imported Upstream version 0.3.0

---
+++ Explorer/dwv/viewers/mobile/appgui.js
@@ -0,0 +1,100 @@
+/** 
+ * Application GUI.
+ */
+
+// Window
+dwv.gui.getWindowSize = dwv.gui.base.getWindowSize;
+// Progress
+dwv.gui.displayProgress = dwv.gui.base.displayProgress;
+// Select
+dwv.gui.refreshSelect = dwv.gui.base.refreshSelect;
+// Slider
+dwv.gui.Slider = dwv.gui.base.Slider;
+// Tags gui
+dwv.gui.DicomTags = dwv.gui.base.DicomTags;
+
+// Loaders
+dwv.gui.Loadbox = dwv.gui.base.Loadbox;
+// File loader
+dwv.gui.FileLoad = dwv.gui.base.FileLoad;
+// Url loader
+dwv.gui.UrlLoad =  dwv.gui.base.UrlLoad;
+
+// Toolbox 
+dwv.gui.Toolbox = function (app)
+{
+    var base = new dwv.gui.base.Toolbox(app);
+    
+    this.setup = function (list)
+    {
+        base.setup(list);
+        
+        // toolbar
+        var buttonClass = "ui-btn ui-btn-inline ui-btn-icon-notext ui-mini"; 
+        
+        var open = document.createElement("a");
+        open.href = "#popupOpen";
+        open.setAttribute("class", buttonClass + " ui-icon-plus");
+        open.setAttribute("data-rel", "popup");
+        open.setAttribute("data-position-to", "window");
+    
+        var undo = document.createElement("a");
+        undo.setAttribute("class", buttonClass + " ui-icon-back");
+        undo.onclick = app.onUndo;
+    
+        var redo = document.createElement("a");
+        redo.setAttribute("class", buttonClass + " ui-icon-forward");
+        redo.onclick = app.onRedo;
+    
+        var toggleInfo = document.createElement("a");
+        toggleInfo.setAttribute("class", buttonClass + " ui-icon-info");
+        toggleInfo.onclick = app.onToggleInfoLayer;
+    
+        var tags = document.createElement("a");
+        tags.href = "#tags_page";
+        tags.setAttribute("class", buttonClass + " ui-icon-grid");
+    
+        var node = document.getElementById("toolbar");
+        node.appendChild(open);
+        node.appendChild(undo);
+        node.appendChild(redo);
+        node.appendChild(toggleInfo);
+        node.appendChild(tags);
+        $("#toolbar").trigger("create");
+    };
+    this.display = function (flag)
+    {
+        base.display(flag);
+    };
+    this.initialise = function (list)
+    {
+        base.initialise(list);
+    };
+};
+
+// Window/level
+dwv.gui.WindowLevel = dwv.gui.base.WindowLevel;
+// Draw
+dwv.gui.Draw = dwv.gui.base.Draw;
+// Livewire
+dwv.gui.Livewire = dwv.gui.base.Livewire;  
+// ZoomAndPan
+dwv.gui.ZoomAndPan = dwv.gui.base.ZoomAndPan;
+// Scroll
+dwv.gui.Scroll = dwv.gui.base.Scroll;
+// Filter
+dwv.gui.Filter = dwv.gui.base.Filter;
+
+// Filter: threshold
+dwv.gui.Threshold = dwv.gui.base.Threshold;
+// Filter: sharpen
+dwv.gui.Sharpen = dwv.gui.base.Sharpen;
+// Filter: sobel
+dwv.gui.Sobel = dwv.gui.base.Sobel;
+
+// Undo/redo
+dwv.gui.Undo = dwv.gui.base.Undo;
+// Help
+dwv.gui.appendHelpHtml = dwv.gui.base.appendHelpHtml;
+// Version
+dwv.gui.appendVersionHtml = dwv.gui.base.appendVersionHtml;
